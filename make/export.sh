b1ec0fcec622ef5a907e5dd648c8508585150779
adrikei <adrikei@228cc317-f420-46da-b5b0-5f10870c81e6>
2012-06-13 23:18:35 +0000

    Added Script for building deliverable compiler
    git-svn-id: https://paninij.svn.sourceforge.net/svnroot/paninij/panc/trunk@497 228cc317-f420-46da-b5b0-5f10870c81e6

---
+++ make/export.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+rm -rf panc
+rm -rf panc.zip
+
+mkdir panc
+mkdir panc/lib
+mkdir panc/bin
+mkdir panc/editors
+
+cp -r ../dist panc/lib/dist
+cp -r ../licenses panc/licenses
+cp export/panc.sh panc/bin/panc
+cp export/panc.bat panc/bin/
+cp export/panini_rt.jar panc/lib/
+cp export/javac panc/lib/dist/bootstrap/bin/
+cp export/panini-mode.el panc/editors/
+cp export/panini.vim panc/editors/
+cp -r ../examples panc/
+cp ../README panc/
+
+find panc -name '.svn' -exec rm -Rf '{}' ';' 2>/dev/null
+zip -r panc.zip panc
