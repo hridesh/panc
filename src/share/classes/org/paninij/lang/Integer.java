becc947b36aebd80701f1580172286f79c24f580
hridesh <hridesh@228cc317-f420-46da-b5b0-5f10870c81e6>
2012-06-28 01:33:20 +0000

    The Integer class wraps a value of the primitive type int in an object. Compared to the class java.lang.Integer that inherits from java.lang.Object, this class inherits from java.lang.Object and thus it requires explicit specification of the owner module of an instance of this class.
    git-svn-id: https://paninij.svn.sourceforge.net/svnroot/paninij/panc/trunk@553 228cc317-f420-46da-b5b0-5f10870c81e6

---
+++ src/share/classes/org/paninij/lang/Integer.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the Panini project at Iowa State University.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * 
+ * For more details and the latest version of this code please see
+ * http://paninij.org
+ * 
+ * Contributor(s): Hridesh Rajan
+ */
+
+package org.paninij.lang;
+
+/***
+ * The Integer class wraps a value of the primitive type int in an object. 
+ * An object of type Integer contains a single field whose type is int.
+ * 
+ * In addition, this class provides several methods for converting an int 
+ * to a String and a String to an int, as well as other constants and 
+ * methods useful when dealing with an int. 
+ * 
+ * @param <OWNER> - The owner module of an instance of this class.
+ * @since 1.0
+ */
+public final class Integer <OWNER> extends Object <OWNER>{
+ //TODO: to be implemented.
+}
