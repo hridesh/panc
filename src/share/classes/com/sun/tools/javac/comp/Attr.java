d85cae926b4cc18a2f8c302bd9f0ee45fb067187
Hridesh Rajan <hridesh@users.sourceforge.net>
2012-09-12 22:16:33 -0500

    Feature Requests item #3548574 Allow Panini programs to process command-line arguments.

--- src/share/classes/com/sun/tools/javac/comp/Attr.java
+++ src/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -716,56 +716,56 @@ public class Attr extends JCTree.Visitor {
     }
     
     // Panini code
-    public List<JCStatement> transWiring(JCMethodInvocation mi, Map<Name, Name> variables){
+    private List<JCStatement> transWiring(final JCMethodInvocation mi, final Map<Name, Name> variables){
     	if(variables.get(names
-				.fromString(mi.meth.toString()))==null){
+    			.fromString(mi.meth.toString()))==null){
     		log.error(mi.pos(), "module.array.type.error", mi.meth);
     	}
     	ClassSymbol c = (ClassSymbol) rs
-				.findType(env, variables.get(names
-						.fromString(mi.meth.toString())));
+    			.findType(env, variables.get(names
+    					.fromString(mi.meth.toString())));
     	ListBuffer<JCStatement> assigns = new ListBuffer<JCStatement>();
-		if (mi.args.length() != syms.moduleparams.get(c).length()) {
-			log.error(mi.pos(), "arguments.of.wiring.mismatch");
-		} else {
-			for (int j = 0; j < mi.args.length(); j++) {
-				JCAssign newAssign = make
-						.at(mi.pos())
-						.Assign(make.Select(mi.meth,
-								syms.moduleparams.get(c).get(j)
-										.getName()), mi.args.get(j));
-				JCExpressionStatement assignAssign = make
-						.Exec(newAssign);
-				assigns.append(assignAssign);
-			}
-		}
-		return assigns.toList();
+    	if (mi.args.length() != syms.moduleparams.get(c).length()) {
+    		log.error(mi.pos(), "arguments.of.wiring.mismatch");
+    	} else {
+    		for (int j = 0; j < mi.args.length(); j++) {
+    			JCAssign newAssign = make
+    					.at(mi.pos())
+    					.Assign(make.Select(mi.meth,
+    							syms.moduleparams.get(c).get(j)
+    							.getName()), mi.args.get(j));
+    			JCExpressionStatement assignAssign = make
+    					.Exec(newAssign);
+    			assigns.append(assignAssign);
+    		}
+    	}
+    	return assigns.toList();
     }
 
-    public void visitModuleDef(JCModuleDecl tree){
-        if (tree.needsDefaultRun){
-        	List<JCClassDecl> wrapperClasses = moduleInternal.generateClassWrappers(tree, env, rs);
-        	enter.classEnter(wrapperClasses, env.outer);
-//        	System.out.println(wrapperClasses);
-        	attribClassBody(env, tree.sym);
-            tree.computeMethod.body = moduleInternal.generateComputeMethodBody(tree);
-        	}
-        else
-        	attribClassBody(env, tree.sym);
-        for(JCTree def : tree.defs){
-        	if(def.getTag() == Tag.METHODDEF){
-        		for(JCVariableDecl param : ((JCMethodDecl)def).params){
-        			if(param.type.tsym.isModule&&!((JCMethodDecl)def).name.toString().contains("$Original")){
-        				log.error("procedure.argument.illegal", param, ((JCMethodDecl)def).name.toString(), tree.name);
-        			}
-        		}
-        		
-        	}
-        }
-//        System.out.println(tree);
+    public final void visitModuleDef(final JCModuleDecl tree){
+    	if (tree.needsDefaultRun){
+    		List<JCClassDecl> wrapperClasses = moduleInternal.generateClassWrappers(tree, env, rs);
+    		enter.classEnter(wrapperClasses, env.outer);
+    		//        	System.out.println(wrapperClasses);
+    		attribClassBody(env, tree.sym);
+    		tree.computeMethod.body = moduleInternal.generateComputeMethodBody(tree);
+    	}
+    	else
+    		attribClassBody(env, tree.sym);
+    	for(JCTree def : tree.defs){
+    		if(def.getTag() == Tag.METHODDEF){
+    			for(JCVariableDecl param : ((JCMethodDecl)def).params){
+    				if(param.type.tsym.isModule&&!((JCMethodDecl)def).name.toString().contains("$Original")){
+    					log.error("procedure.argument.illegal", param, ((JCMethodDecl)def).name.toString(), tree.name);
+    				}
+    			}
+
+    		}
+    	}
+    	//        System.out.println(tree);
     }
 
-    public void visitSystemDef(JCSystemDecl tree){
+    public final void visitSystemDef(final JCSystemDecl tree){
     	ListBuffer<JCStatement> decls = new ListBuffer<JCStatement>();
     	ListBuffer<JCStatement> inits = new ListBuffer<JCStatement>();
     	ListBuffer<JCStatement> assigns = new ListBuffer<JCStatement>();
@@ -775,38 +775,42 @@ public class Attr extends JCTree.Visitor {
     	Map<Name, Name> variables = new HashMap<Name, Name>();
     	Map<Name, Integer> modArrays = new HashMap<Name, Integer>();
     	for(int i=0;i <tree.body.stats.length();i++){
-    		if(tree.body.stats.get(i).getTag() == VARDEF){
-    			JCVariableDecl vdecl = (JCVariableDecl)tree.body.stats.get(i);
-    			if(syms.modules.containsKey(names.fromString(vdecl.vartype.toString())))
+    		JCStatement currentSystemStmt = tree.body.stats.get(i);
+    		Tag systemStmtKind = currentSystemStmt.getTag();
+    		if(systemStmtKind == VARDEF){
+    			JCVariableDecl vdecl = (JCVariableDecl) currentSystemStmt;
+    			Name vdeclTypeName = names.fromString(vdecl.vartype.toString());
+    			if(syms.modules.containsKey(vdeclTypeName))
     				processModuleDef(tree, decls, inits, submits, starts, joins, variables, vdecl);
-    			else if(syms.libclasses.containsKey(names.fromString(vdecl.vartype.toString())))
+    			else if(syms.libclasses.containsKey(vdeclTypeName))
     				processLibModuleDef(tree, decls, inits, submits, starts, joins,	variables, vdecl);
     			else{
     				if(vdecl.vartype.getTag()==MODULEARRAY){
     					processModuleArray(tree, decls, assigns, submits, starts, joins,	variables, modArrays, vdecl);
     				}
     				else{
-    					if(vdecl.vartype.getTag()==TYPEIDENT ||vdecl.vartype.toString().equals("String")){
+    					if(vdecl.vartype.getTag()==TYPEIDENT || vdecl.vartype.toString().equals("String")){
     						vdecl.mods.flags |=FINAL;
     						decls.add(vdecl);
     					} else
     						log.error(vdecl.pos(), "only.primitive.types.or.strings.allowed");
     				}
     			}
-    		} else if(tree.body.stats.get(i).getTag() == EXEC){
-    			if(((JCExpressionStatement)tree.body.stats.get(i)).expr.getTag()==APPLY){
-    				processModuleWiring(tree, assigns, variables, i);
+    		} else if(systemStmtKind == EXEC){
+    			JCExpressionStatement currentExprStmt = (JCExpressionStatement) currentSystemStmt;
+    			if(currentExprStmt.expr.getTag()==APPLY){
+    				processModuleWiring(currentExprStmt.expr, assigns, variables);
     			}
-    		}else if(tree.body.stats.get(i).getTag() == FOREACHLOOP)
-    			processForEachLoop(tree, assigns, variables, i);
-    		else if(tree.body.stats.get(i).getTag()==MAAPPLY)
-    			processModuleArrayWiring(tree, assigns, variables, modArrays, i);
+    		}else if(systemStmtKind == FOREACHLOOP)
+    			processForEachLoop((JCEnhancedForLoop) currentSystemStmt, assigns, variables);
+    		else if(systemStmtKind == MAAPPLY)
+    			processModuleArrayWiring((JCModuleArrayCall) currentSystemStmt, assigns, variables, modArrays);
     		else  			
     			throw new AssertionError("Invalid statement gone through the parser");
     	}
 
     	List<JCStatement> mainStmts = decls.appendList(inits).appendList(assigns).appendList(starts).appendList(joins).appendList(submits).toList();
-    	JCMethodDecl maindecl = addMainMethod(tree.sym, tree.body, mainStmts);
+    	JCMethodDecl maindecl = createMainMethod(tree.sym, tree.body, tree.params, mainStmts);
     	tree.defs = tree.defs.append(maindecl);
 
     	tree.switchToClass();
@@ -814,7 +818,7 @@ public class Attr extends JCTree.Visitor {
     	memberEnter.memberEnter(maindecl, env);
     }
 
-				private JCMethodDecl addMainMethod(final ClassSymbol containingClass, final JCBlock methodBody, final List<JCStatement> mainStmts) {
+				private final JCMethodDecl createMainMethod(final ClassSymbol containingClass, final JCBlock methodBody, final List<JCVariableDecl> params, final List<JCStatement> mainStmts) {
 					Type arrayType = new ArrayType(syms.stringType, syms.arrayClass);
     	MethodSymbol msym = new MethodSymbol(
     			PUBLIC|STATIC,
@@ -827,17 +831,19 @@ public class Attr extends JCTree.Visitor {
     					),
     					containingClass
     			);
-    	JCVariableDecl mainArg = make.Param( names.fromString("args"), arrayType, msym);
     	JCMethodDecl maindecl = make.MethodDef(msym, methodBody);
-    	maindecl.params = List.<JCVariableDecl>of(mainArg);
+    	JCVariableDecl mainArg = null; 
+    	if(params.length() == 0) {
+    		mainArg = make.Param( names.fromString("args"), arrayType, msym);
+    		maindecl.params = List.<JCVariableDecl>of(mainArg);
+    	} else maindecl.params = params;
+    	
     	maindecl.body.stats = mainStmts;
 					return maindecl;
 				}
 
-				private void processModuleWiring(JCSystemDecl tree,
-						ListBuffer<JCStatement> assigns, Map<Name, Name> variables, int i) {
-					JCMethodInvocation mi = (JCMethodInvocation) ((JCExpressionStatement) tree.body.stats
-							.get(i)).expr;
+				private void processModuleWiring(final JCExpression wiring, final ListBuffer<JCStatement> assigns, final Map<Name, Name> variables) {
+					JCMethodInvocation mi = (JCMethodInvocation) wiring;
 					try{
 						assigns.appendList(transWiring(mi, variables));
 					}catch (NullPointerException e){
@@ -845,11 +851,9 @@ public class Attr extends JCTree.Visitor {
 					}
 				}
 
-				private void processModuleArrayWiring(JCSystemDecl tree,
+				private void processModuleArrayWiring(JCModuleArrayCall mi,
 						ListBuffer<JCStatement> assigns, Map<Name, Name> variables,
-						Map<Name, Integer> modArrays, int i) {
-					JCModuleArrayCall mi = (JCModuleArrayCall) tree.body.stats
-							.get(i);
+						Map<Name, Integer> modArrays) {
 					if(!variables.containsKey(names
 							.fromString(mi.name.toString()))){
 						log.error(mi.pos(), "symbol.not.found");
@@ -885,9 +889,7 @@ public class Attr extends JCTree.Visitor {
 					}
 				}
 
-				private void processForEachLoop(JCSystemDecl tree,
-						ListBuffer<JCStatement> assigns, Map<Name, Name> variables, int i) {
-					JCEnhancedForLoop loop = (JCEnhancedForLoop) tree.body.stats.get(i);
+				private void processForEachLoop(JCEnhancedForLoop loop, ListBuffer<JCStatement> assigns, Map<Name, Name> variables) {
 					ClassSymbol c = syms.modules.get(names.fromString(loop.var.vartype.toString()));
 					if(c==null){
 						log.error(loop.pos(), "module.array.type.error", loop.var.vartype);
