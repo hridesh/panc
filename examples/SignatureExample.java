7a90114de8002c2d33d01ec333f64baa114c20ac
Hridesh Rajan <hridesh@users.sourceforge.net>
2013-01-29 17:54:52 -0600

    Write this as a variation of the HelloWorld example.

--- examples/SignatureExample.java
+++ examples/SignatureExample.java
@@ -1,20 +1,35 @@
-signature Sig {
- void sigMethod(); 
+/*
+ * This file is part of the Panini project at Iowa State University.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * 
+ * For more details and the latest version of this code please see
+ * http://paninij.org
+ * 
+ * Contributor(s): Hridesh Rajan
+ */
+
+signature Stream {
+ void write(String s); 
 }
 
-capsule Mod () implements Sig {
- void sigMethod() {
-  System.out.println("Hello World, Signature Style!");
- }
+capsule Console () implements Stream {
+	void write(String s) { System.out.println(s); }
 }
 
-capsule Client (Sig s) {
- void run () {
-  s.sigMethod(); 
- }
+capsule Greeter (Stream s) {
+	void run(){ s.write("Hello World, Signature Style!"); }
 }
 
 system SignatureExample {
- Mod m; Client c;
- c(m);
+	Console c; Greeter g;
+	g(c);
 }
