5b09e2981e8642e0b34ba3e477dd46dce2da4be5
Hridesh Rajan <hridesh@users.sourceforge.net>
2013-02-04 12:37:25 -0600

    Revised and simplified.

--- examples/Barbershop2.java
+++ examples/Barbershop2.java
@@ -1,118 +1,91 @@
+/*
+ * This file is part of the Panini project at Iowa State University.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * 
+ * For more details and the latest version of this code please see
+ * http://paninij.org
+ * 
+ * Contributor(s): Hridesh Rajan, Eric Lin, Rex Fernando
+ */
+
 import java.util.Queue;
 import java.util.LinkedList;
 
-    class BooleanC {
-        boolean v;
-        public BooleanC(boolean v) { this.v = v; }
-        public boolean value() { return v; }
-    }
-
-    class IntC {
-        int v;
-        public IntC(int v) { this.v = v; }
-        public int value() { return v; }
-    }
-
-
-    class Customer {
-        private int id;
-        public Customer(int id) { this.id = id; }
-        public int getID() { return id; }
-    }
-
-    class MaybeCustomer {
-        private Customer c;
-        public MaybeCustomer(Customer c) { this.c = c; }
-        public Customer getCustomer() { return c; }
-    }
-  
-capsule Barber(WaitingRoom r, boolean isSleeping) {
-          
-    void wake(Customer c){
-        isSleeping = false;
-        System.out.println("Barber Woke up");
-        work(c);
-        Customer n = r.whosNext().getCustomer();
-        while (n!=null) {
-            work(n);
-            n = r.whosNext().getCustomer();
-        }
-        sleep();
-        
-    }
-          
-    void sleep(){
-        System.out.println("Barber went to sleep");
-        isSleeping = true;
-    }
-          
-    void work(Customer c){
-        System.out.println("Barber working on customer " + c.getID());  
-        yield(1000);
-    }
-          
-    BooleanC isSleeping(){
-        return new BooleanC(isSleeping);
-    }
-          
+class Customer {
+	private String name;
+	public Customer(String name) { this.name = name; }
+	public String getName() { return name; }
 }
-  
+
+capsule Barber(WaitingRoom r, boolean isSleeping, int totalCustomers) {
+	void run() {
+		int totalHairCuts = 0; 
+		while (totalHairCuts < totalCustomers) {
+			if (!isSleeping) {
+				Customer c = r.whosNext();
+				if(!c.equals(null)) { 
+					System.out.println("Barber working on customer " + c.getName());
+					totalHairCuts++;
+				} else {
+					System.out.println("Barber went to sleep");
+					isSleeping = true;
+				}
+			} else yield(1000);
+		}
+	}
+	void wakeIfSleeping() { 
+		if(isSleeping) {
+			isSleeping = false; 
+			System.out.println("Barber Woke up");
+		}
+	}
+}
+
 capsule WaitingRoom(int cap) {
-  
-    Queue<Customer> queue = new LinkedList<Customer>();
-    int idCounter = 0;
-  
-    IntC incIdCount() { return new IntC(idCounter++); }
-  
-    BooleanC sit(Customer c){
-        if (queue.size()<cap) {
-            queue.offer(c);
-            System.out.println("Customer " + c.getID() + " Sitting in waiting room");
-            return new BooleanC(true);
-        }
-        else
-            return new BooleanC(false);
-    }
-          
-    MaybeCustomer whosNext() {
-        return new MaybeCustomer(queue.poll());
-    }
+	Queue<Customer> queue = new LinkedList<Customer>();
+	void sit(Customer c){
+		if (queue.size()<cap) {
+			queue.offer(c);
+			System.out.println("Customer " + c.getName() + " Sitting in waiting room");
+		}
+		else 
+			System.out.println("Waiting room is full, so " + "customer" + c.getName() + " is leaving");
+	}
+	Customer whosNext() {
+		return queue.poll();
+	}
 }
-  
-capsule Customers(Barber b, WaitingRoom r) {
-    int idCounter = 0;
-  
-    void run() {
-        while (true) {
-            Customer c = new Customer(r.incIdCount().value());
-            System.out.println("Customer " + c.getID() + " wants haircut");
-            if (!b.isSleeping().value()) {
-                trySit(c);
-            } else {
-                System.out.println("Customer is waking barber up");
-                b.wake(c);
-            }
-            yield(1000);
-        }
-    }
-  
-    void trySit(Customer c) {
-        System.out.println("Barber is busy, trying to sit down");
-        if(!r.sit(c).value()) {
-            System.out.println("Waiting room is full, so leaving");
-        }
-    }
-}
-  
-system Barbershop2 {
-    Barber b;
-    WaitingRoom w;
-    Customers c[5];
-  
-    b(w, true);
-    w(10);       
 
-    for(Customers cs : c){
-        cs(b, w);
-    }
+capsule Customers(Barber b, WaitingRoom r, String[] customerNames) {
+	int idCounter = 0;
+	void run() {
+		for (int i = 0; i < customerNames.length; i++) {
+			Customer c = new Customer(customerNames[i]);
+			System.out.println("Customer " + c.getName() + " wants haircut.");
+			b.wakeIfSleeping();
+			r.sit(c);
+			yield(1000);
+		}
+	}
+}
+
+system Barbershop2 {
+	Barber b;
+	WaitingRoom w;
+	Customers gs[2];
+
+	b(w, true, 8);
+	w(10);       
+
+	gs[0](b,w, new String[]{"Hridesh", "Eric", "Steve", "Sarah"});
+	gs[1](b,w, new String[]{"Ganesha", "Rex", "Yuheng", "Lorand"});
 }
