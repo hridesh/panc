b5def829ac498f1c0ec18b535e707275da74148c
shrader <shrader@iastate.edu>
2013-02-06 19:18:32 -0600

    Fixed a bug where redundant org.panini.runtime.* and duck imports were being generated. This was due to a block of code which was being executed on each call to visitCapsuleDef.
    This block was shuffled off to a method named
    setRuntimeImports(Env<AttrContext> env)
    and is now called on topEnv in the method visitTopLevel. This asserts
    that the block is executed once per compilation unit. It also fixes the
    issue where no runtime imports were generated for compilation units
    containing only a single system declaration.

--- src/share/classes/com/sun/tools/javac/comp/Enter.java
+++ src/share/classes/com/sun/tools/javac/comp/Enter.java
@@ -308,6 +308,8 @@ public class Enter extends JCTree.Visitor {
         }
         tree.packge.complete(); // Find all classes in package.
         Env<AttrContext> topEnv = topLevelEnv(tree);
+        
+        setRuntimeImports(topEnv);
 
         // Save environment of package-info.java file.
         if (isPkgInfo) {
@@ -686,10 +688,8 @@ public class Enter extends JCTree.Visitor {
         tree.switchToClass();
     }
         
-    public void visitCapsuleDef(JCCapsuleDecl tree){
-    	if((tree.mods.flags & Flags.INTERFACE) !=0){
-    		tree.needsDefaultRun= false;
-    	}
+    
+    private void setRuntimeImports(Env<AttrContext> env){
     	JCExpression pid = make.Ident(names.fromString("java"));
     	pid = make.Select(pid, names.fromString("util"));
     	pid = make.Select(pid, names.fromString("concurrent"));
@@ -717,6 +717,14 @@ public class Enter extends JCTree.Visitor {
     	pid = make.Select(pid, names.fromString("types"));
     	pid = make.Select(pid, names.fromString("Panini$Duck$Void"));
     	env.toplevel.defs = env.toplevel.defs.prepend(make.Import(pid, false));
+    }
+    
+    
+    public void visitCapsuleDef(JCCapsuleDecl tree){
+    	if((tree.mods.flags & Flags.INTERFACE) !=0){
+    		tree.needsDefaultRun= false;
+    	}
+    	
     	
     	Symbol owner = env.info.scope.owner;
         Scope enclScope = enterScope(env);
