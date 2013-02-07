9573dd46280c7585a097e9dc26bbab600d0af999
Hridesh Rajan <hridesh@users.sourceforge.net>
2013-02-07 00:38:30 -0600

    A script to run basic regression test on compiler.

---
+++ examples/testExamples.sh
@@ -0,0 +1,23 @@
+EXAMPLES="Barbershop Barbershop2 HelloWorld Histogram Philosophers Pi SequentialConsistency SignatureExample"
+for EXAMPLE in $EXAMPLES 
+ do
+  echo "Testing $EXAMPLE."
+  ../bin/panc $EXAMPLE.java
+  ../bin/panini $EXAMPLE
+  rm *.class
+ done
+
+echo "Testing AddressBook"
+cd AddressBook
+../../bin/panc -cp .:htmlparser.jar:../../lib/panini_rt.jar *.java
+../../bin/panini -cp .:htmlparser.jar:../../lib/panini_rt.jar AddressBook 
+rm *.class 
+cd -
+
+#echo "Testing GA"
+#cd GA
+#../../bin/panc AILib/*.java GA.java
+#../../bin/panini GA
+#rm *.class 
+#cd - 
+
