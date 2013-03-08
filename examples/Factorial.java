3773a0caa7e96146cc7d2cdde99867bae6812a95
eyhlin <eylin@iastate.edu>
2013-03-08 11:40:41 -0600

    Added Factorial example to repository

---
+++ examples/Factorial.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the Panini project at Iowa State University.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * 
+ * For more details and the latest version of this code please see
+ * http://paninij.org
+ * 
+ * Contributor(s): Hridesh Rajan
+ */
+
+class Number{
+	int number;
+	Number(int number){ this.number = number; 
+	}
+	int value(){ return number;}
+}
+
+capsule Fact () {
+	Number fact(int current) { 
+	        if(current == 0) {
+		  return new Number(0);
+		}
+	 	return new Number(current + fact(current-1).value()); 
+	}
+}
+
+capsule Main (Fact c) {
+	void run(){ System.out.println(c.fact(10).value()); }
+}
+
+system Factorial {
+	Main m; Fact c;
+	m(c);
+}
