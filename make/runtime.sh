b15947228bd16094f4a07b7f7220ae2d4b482e17
Hridesh Rajan <hridesh@users.sourceforge.net>
2013-05-06 10:43:07 -0500

    Including lang package in runtime export.

--- make/runtime.sh
+++ make/runtime.sh
@@ -10,7 +10,8 @@ mkdir jar/org/paninij
 
 cd jar/org/paninij
 cp -r ../../../../build/bootstrap/classes/org/paninij/runtime .
-cd ../..	
+cp -r ../../../../build/bootstrap/classes/org/paninij/lang .
+cd ../..
 jar cvf panini_rt.jar *
 
 cp panini_rt.jar ../export
