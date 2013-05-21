2f9ab794571e7ffec2a9c54b2b4879450b9122d9
Sean Mooney <bable4@gmail.com>
2013-05-21 15:41:29 -0500

    Updated PANC_LIB in panc.bat.
    * Fixes broken path issues from creating the panc frontend.

--- make/export/panc.bat
+++ make/export/panc.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set PANC_HOME=%~dp0\..
-set LIB_HOME=%PANC_HOME%\lib\dist\bootstrap
+set LIB_HOME=%PANC_HOME%\lib\dist\lib
 
 rem TODO parse out the args and find -J opts, stick in here
 set JAVA_OPTS=
@@ -15,9 +15,9 @@ rem        sed -e 's|Class-Path: *||' -e 's|\([a-z]*\.jar\) *|'"$mylib"'/\1:|g'`
 rem    bcp="$mylib/javac.jar:$cp"
 rem fi
 rem TODO need to set the bcp from the manifest in the javac.jar
-set BCP=%LIB_HOME%\lib\javac.jar
+set BCP=%LIB_HOME%\panc.jar
 if NOT "%BCP%"=="" set BCP=-Xbootclasspath/p:%BCP%
 
 
 rem "java" "${bcp:+-Xbootclasspath/p:"$bcp"}" ${ea} ${javaOpts} -jar "${mylib}/javac.jar" ${toolOpts}
-java "%BCP%" -ea:com.sun.tools... %JAVA_OPTS% -jar "%LIB_HOME%\lib\javac.jar" -source 1.6 -target 1.6 -cp "%PANC_HOME%\lib\panini_rt.jar";. -Xlint:-options %TOOL_OPTS%
+java "%BCP%" -ea:com.sun.tools... %JAVA_OPTS% -jar "%LIB_HOME%\panc.jar" -source 1.6 -target 1.6 -cp "%PANC_HOME%\lib\panini_rt.jar";. -Xlint:-options %TOOL_OPTS%
