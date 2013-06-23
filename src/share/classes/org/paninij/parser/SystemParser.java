666f231f9c857678e9a84c2f9cb0dd1b4f359048
Sean Mooney <bable4@gmail.com>
2013-06-23 10:51:15 -0500

    [WORKAROUND] Temporary fix for issues parsing indexed wiring expressions.
    * The system parser currently parses indexed wiring with the 'indexed'
      expression as a ArrayAccess expression (which is amoung other issues,
      redundant.) Fix the problem by 'unwrapping' the inner expression
      array access as use it's 'indexed' expression (which is what we want)
      instead.

--- src/share/classes/org/paninij/parser/SystemParser.java
+++ src/share/classes/org/paninij/parser/SystemParser.java
@@ -69,6 +69,7 @@ import com.sun.tools.javac.parser.Tokens;
 import com.sun.tools.javac.parser.Tokens.Token;
 import com.sun.tools.javac.parser.Tokens.TokenKind;
 import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCArrayAccess;
 import com.sun.tools.javac.tree.JCTree.JCBinary;
 import com.sun.tools.javac.tree.JCTree.JCBlock;
 import com.sun.tools.javac.tree.JCTree.JCErroneous;
@@ -81,6 +82,7 @@ import com.sun.tools.javac.tree.JCTree.JCPrimitiveTypeTree;
 import com.sun.tools.javac.tree.JCTree.JCStatement;
 import com.sun.tools.javac.tree.JCTree.JCSystemDecl;
 import com.sun.tools.javac.tree.JCTree.JCVariableDecl;
+import com.sun.tools.javac.tree.JCTree.Tag;
 import com.sun.tools.javac.tree.TreeInfo;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.util.Assert;
@@ -946,6 +948,13 @@ public class SystemParser {
             JCIdent nameOfArray, JCExpression indexExpression) {
         if ((mode & EXPR) != 0 && (token.kind == LPAREN)) {
             List<JCExpression> args = parseArgumentList();
+            //TODO: Remove when the parser is fixed.
+            //unwrap the inner indexed if there is one.
+            if (t.getTag() == Tag.INDEXED) {
+                System.err.println("Warning found an ArrayAcces expression inside a capsule array wiring expression." +
+                    "\nUnwrapping the inner expression -- FIX THE PARSER");
+                t = ((JCArrayAccess)t).indexed;
+            }
             return F.at(token.pos).CapsuleArrayCall(nameOfArray.getName(),
                     indexExpression, nameOfArray, args);
         }
