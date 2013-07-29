d344843f1ef25f5b00e47e7e491b7cb035094b01
shrader <shrader@iastate.edu>
2013-07-29 15:00:33 -0600

    updated the Panini Windows batch file to work properly with the newly-unified systems/capsules

--- make/export/panini.bat
+++ make/export/panini.bat
@@ -6,7 +6,27 @@ set LIB_HOME=%PANC_HOME%\lib\dist\bootstrap
 rem TODO parse out the args and find -J opts, stick in here
 set JAVA_OPTS=
 rem TODO put only the non -J opts in here
-set TOOL_OPTS=%*
+set TOOL_OPTS=
+set CP=%PANC_HOME%\lib\panini_rt.jar
+
+:Loop
+IF [%1]==[] GOTO Skip
+set arg=%1
+IF %arg%==-cp GOTO Append
+IF %arg%==-classpath GOTO Append
+IF NOT [%arg:~0,1%]==[-] set arg=%arg%$thread
+set TOOL_OPTS=%TOOL_OPTS% %arg%
+SHIFT
+GOTO Loop
+:Append
+set TOOL_OPTS=%TOOL_OPTS% %arg%
+SHIFT
+set arg=%1
+set TOOL_OPTS=%TOOL_OPTS% %arg%
+SHIFT
+set CP=%CP%:%arg%
+GOTO Loop
+:Skip
 
 rem if [ "$LANGTOOLS_USE_BOOTCLASSPATH" != "no" ]; then
 rem    cp=`unzip -c "$mylib/javac.jar" META-INF/MANIFEST.MF |
@@ -18,6 +38,5 @@ rem TODO need to set the bcp from the manifest in the javac.jar
 set BCP=%LIB_HOME%\lib\javac.jar
 if NOT "%BCP%"=="" set BCP=-Xbootclasspath/p:%BCP%
 
-
 rem "java" "${bcp:+-Xbootclasspath/p:"$bcp"}" ${ea} ${javaOpts} -jar "${mylib}/javac.jar" ${toolOpts}
-java %JAVA_OPTS% -cp "%PANC_HOME%\lib\panini_rt.jar";. %TOOL_OPTS%
+java %JAVA_OPTS% -cp %CP%;. %TOOL_OPTS%
