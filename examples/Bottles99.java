c8c95a853c89bb3d963ada5b72af8857848c051d
Dr. Hridesh Rajan <hridesh@cs.iastate.edu>
2014-07-14 17:58:39 -0500

    Small fix.

--- examples/Bottles99.java
+++ examples/Bottles99.java
@@ -19,10 +19,10 @@
 
 capsule Bottles99 {
     void run() {
-	for(int i=99; i>0; i--) 
+	for(int i=99; i>1; i--) 
 	    System.out.println(nonZero(i));
 	System.out.println("1 bottle of beer on the wall, 1 bottle of beer.\n" +
-		"Take one down and pass it around, no more bottles of beer on the wall.\n" +
+		"Take one down and pass it around, no more bottles of beer on the wall.\n\n" +
 		"No more bottles of beer on the wall, no more bottles of beer.\n" +
 		"Go to the store and buy some more, 99 bottles of beer on the wall.");
     }
